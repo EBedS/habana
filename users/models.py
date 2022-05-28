20b3bbe24200f3e2fa24133d186945ceb0a93a6a
Eduardo Bedecarratz Salvadores <eduardobedecarratz@gmail.com>
2022-05-28 19:35:29 -0400

    19:35

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+	def natural_key(self):
+		return self.username
+
+
+

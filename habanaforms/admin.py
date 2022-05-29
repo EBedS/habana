9f47eb08704cb0b897ca45fc6ed8e403a5e3a9e6
Eduardo Bedecarratz Salvadores <eduardobedecarratz@gmail.com>
2022-05-28 21:20:54 -0400

    admin

--- habanaforms/admin.py
+++ habanaforms/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from habanaforms.models import *
 
 # Register your models here.
+admin.site.register(HabanaFormFieldChoices)
+admin.site.register(HabanaForm)
+admin.site.register(HabanaFormField)
+admin.site.register(HabanaFormResponse)
+
+
+
+
